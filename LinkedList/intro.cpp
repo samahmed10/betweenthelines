3b0656d820119fcbbd4c85cfa4e10714b706f21a
Sameer Ahmed <sahmed653@gmail.com>
2018-06-08 23:50:41 +0530

    Dummy

--- LinkedList/intro.cpp
+++ LinkedList/intro.cpp
@@ -63,7 +63,51 @@ void deleteBack(struct node **head)
 	}
 }
 	
+void deleteKey(struct node **head,int data)
+{
+	struct node *temp = *head;
+	if(temp != NULL && temp->data == data)
+	{
+		*head = temp->next;
+		free(temp);
+		return;
+	}
+	struct node *prev;
+	while(temp != NULL && temp->data != data)
+	{
+		prev = temp;
+		temp = temp->next;
+	}
+	if(temp == NULL)		
+		return;
+	prev->next = temp->next;
+	free(temp);
+	return;
+}
 
+void deletePos(struct node **head,int pos)
+{
+	if(*head == NULL)
+		return;
+	struct node *temp = *head;
+	if(pos == 0)
+	{
+		*head = temp->next;
+		free(temp);
+		return;
+	}
+	for(int i=0;i<pos-1 && temp != NULL;i++)
+	{
+		temp = temp->next;
+	}
+	if(temp == NULL || temp->next == NULL)
+		return;
+	struct node *next = temp->next->next;
+	free(temp->next);
+	temp->next = next;
+} 
+		
+	
 void display(struct node *head)
 {
 	if(head == NULL)
@@ -81,10 +125,15 @@ int main()
 {
 	struct node *head = NULL;
 	display(head);
-	insertFront(&head,1);
-        insertFront(&head,1);
+	insertFront(&head,5);
+        insertFront(&head,6);
+        insertEnd(&head,8);
         insertEnd(&head,1);
-        insertEnd(&head,1);
+	insertFront(&head,2);
+	insertFront(&head,3);
+	display(head);cout<<endl;
+	deletePos(&head,10);
+	display(head);cout<<endl;
 	display(head);cout<<endl;
 	deleteBack(&head);
 	display(head);cout<<endl;
