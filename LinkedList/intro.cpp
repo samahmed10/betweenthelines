2acb056c47c16748e9266bad89e9cf3ed9743102
Sameer Ahmed <sahmed653@gmail.com>
2018-06-09 16:38:16 +0530

    Dummy

--- LinkedList/intro.cpp
+++ LinkedList/intro.cpp
@@ -171,21 +171,124 @@ void printMid(struct node *head)
 	}
 	cout<<"Mid node is "<<slow->data<<endl;
 }
+
+int countnodes(struct node *head)
+{
+	struct node *temp = head;
+	int res = 1;
+	while(temp->next != head)
+	{
+		res ++;
+		temp = temp->next;
+	}
+	return res;
+}
+
+void detectloop(struct node *head)
+{
+	struct node *fast = head,*slow = head;
+	if(head)
+	{
+		while(fast && slow && fast->next)
+		{
+			fast = fast->next->next;
+			slow = slow->next;
+			if(fast == slow)
+			{
+				cout<<"LOOP EXISTS"<<endl;
+				cout<<"There are "<<countnodes(slow)<<" nodes in the loop"<<endl;
+				return;
+			}
+		}
+	}
+	cout<<"NO LOOP"<<endl;
+}
+
+void reverse(struct node **head)
+{
+	struct node *prev = NULL;
+	struct node *curr = *head;
+	struct node *next;
+	while(curr)
+	{
+		next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
+	}
+	*head = prev;
+}
+
+int compareLists(struct node *head1,struct node *head2)
+{
+	struct node *temp1=head1,*temp2=head2;
+	while(temp1 && temp2)
+	{
+		if(temp1->data == temp2->data)
+		{
+			temp1 = temp1->next;		
+			temp2 = temp2->next;
+		}
+		else
+			return 0;
+	}
+	if(!temp1 && !temp2)
+		return 1;
+	return 0;
+}
+
+int checkPalindrome(struct node *head)
+{
+	struct node *fast = head,*slow = head;
+	int res = 0;
+	struct node *prev,*second,*midnode=NULL;
+	if(head && head->next)
+	{
+		while(fast &&  fast->next)
+		{
+			fast = fast->next->next;
+			prev = slow;
+			slow = slow->next;
+		}	
+		
+		if(fast)
+		{
+			midnode = slow;
+			slow = slow->next;
+		}
+		second = slow;
+		prev->next = NULL;
+		reverse(&second);
+		res = compareLists(head,second);
+		reverse(&second);
+		if(midnode)
+		{
+			prev->next = midnode;
+			midnode->next = second;
+		}
+		else
+			prev->next = second;
+	}
+	return res;
+}
+				
 int main()
 {
 	struct node *head = NULL;
 	deleteList(&head);
 	display(head);
-	insertFront(&head,5);
-        insertFront(&head,6);
-        insertEnd(&head,8);
-	//display(head);cout<<endl;
-        //insertEnd(&head,1);
-	//insertFront(&head,2);
-	//insertFront(&head,3);
-	display(head);cout<<endl;
-	cout<<"\n\n";
-	printMid(head);
+	insertFront(&head,7);
+        insertFront(&head,4);
+        insertEnd(&head,6);
+	display(head);
+	reverse(&head);
+	display(head);
+//	head->next->next->next = head;
+	detectloop(head);
+	if(checkPalindrome(head))
+		cout<<"PALINDROME";
+	else
+		cout<<"NOT A PALINDROME";
 	return 0;
 }
 						
